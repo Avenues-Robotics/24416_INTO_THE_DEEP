80401e882aab330fa092d2e545f63819c99577a2
ryder.liou30 <ryder_liou@avenues-ny.org>
2024-12-19 17:04:32 -0500

    Fixed drive class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/nonpushbotAuto/RightfirstnonpushbotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/nonpushbotAuto/RightfirstnonpushbotAuto.java
@@ -11,13 +11,19 @@ import org.firstinspires.ftc.teamcode.hardware.Slides;
 import org.firstinspires.ftc.teamcode.hardware.StartServo;
 
 @Config
-@Autonomous(name="Right Auto")
+@Autonomous(name="Right Auto 1234")
 public class RightfirstnonpushbotAuto extends LinearOpMode {
     Drive drive;
     Slides slides;
     Rotate rotate;
     StartServo startServo;
-
+    public static double ticks_per_degree = 10.72;
+    public static int strafe_1 = 20;
+    public static int rotate_1 = -90;
+    public static int drive_1 = 30;
+    public static int drive_2 = -30;
+    public static int rotate_2 = -90;
+    public static int drive_3 = 70;
     @Override
     public void runOpMode() {
         CRServo rServo = hardwareMap.get(CRServo.class, "rServo");
@@ -25,23 +31,24 @@ public class RightfirstnonpushbotAuto extends LinearOpMode {
         lServo.setDirection(CRServo.Direction.FORWARD);
         rServo.setDirection(CRServo.Direction.REVERSE);
         drive = new Drive(this);
+        drive.TICKS_PER_DEGREE = ticks_per_degree;
         slides = new Slides(this);
         rotate = new Rotate(this);
         startServo = new StartServo(this);
         startServo.start();
         waitForStart();
-        rotate.armRotateL.setPower(0.5);
-        rotate.armRotateR.setPower(0.5);
-        sleep(300);
-        rotate.armRotateL.setPower(0);
-        rotate.armRotateR.setPower(0);
-        startServo.open();
-        drive.drive(0.5, 50);
-        slides.setState("LOW OUTTAKE", rotate);
-        sleep(1000);
-        slides.setState("SLIDES RETRACTED", rotate);
-        sleep(1000);
-        drive.drive(0.5, -50);
+//        rotate.armRotateL.setPower(0.5);
+//        rotate.armRotateR.setPower(0.5);
+//        sleep(300);
+//        rotate.armRotateL.setPower(0);
+//        rotate.armRotateR.setPower(0);
+//        startServo.open();
+        drive.strafe_left(0.3, strafe_1);
+        drive.rotate(0.3, rotate_1);
+        drive.drive(0.5, drive_1);
+        drive.drive(0.5, drive_2);
+        drive.rotate(0.3, rotate_2);
+        drive.drive(0.5, drive_3);
 
 
 
